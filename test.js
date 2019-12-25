42dcf41d08871c2b962809306b698b1281793bfc
luoquanquan <luoquanquan@baidu.com>
2019-12-25 16:40:56 +0800

    根据规范 2.3.1原生 Promise 循环引用会触发报错

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const Promise = require('./promise')
+// const Promise = require('./promise')
 
 const readFile = filePath => new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -16,7 +16,7 @@ readFile('1')
     .then(data => {console.log(data)}, e => {console.log(e)})
 
 const p = new Promise((resolve, reject) => {
-    throw Error('boom~')
+    resolve(1)
 })
 
 p.then(data => {
@@ -25,5 +25,12 @@ p.then(data => {
     console.log('err ', e)
 })
 
+let np = new Promise((resolve) => {
+    resolve()
+})
+let np2 = np.then(() => np2)
+np2.then(null, reason => {
+    console.log(reason);
+})
 
 
