c5744cd2a450bc7abb7b3434becf12d2f235ba07
luoquanquan <luoquanquan@baidu.com>
2019-12-25 17:03:14 +0800

    添加静态方法, 用于测试

--- promise.js
+++ promise.js
@@ -119,4 +119,13 @@ class Promise {
     }
 }
 
+Promise.defer = Promise.deferred = function () {
+    const dfd = {}
+    dfd.promise = new Promise((resolve, reject) => {
+      dfd.resolve = resolve
+      dfd.reject = reject
+    })
+    return dfd
+}
+
 module.exports = Promise
